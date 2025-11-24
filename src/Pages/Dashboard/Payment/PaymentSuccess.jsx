a8c3c1e6ee35fe16930486cf29d0c3e59819da08
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-11-24 15:29:34 +0600

    implement payment status with update data ... paid...

--- src/Pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/Pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const PaymentSuccess = () => {
-    return (
-        <div>
-            <h2 className="text-5xl">Payment Successful.</h2>
-        </div>
-    );
+  const [searchParams] = useSearchParams();
+  const sessionId = searchParams.get("session_id");
+  const axiosSecure = useAxiosSecure();
+  console.log(sessionId);
+  useEffect(() => {
+    if (sessionId) {
+      axiosSecure
+        .patch(`/payment-success?session_id=${sessionId}`)
+        .then((res) => {
+          console.log(res.data);
+        });
+    }
+  }, [sessionId, axiosSecure]);
+  return (
+    <div>
+      <h2 className="text-5xl">Payment Successful.</h2>
+    </div>
+  );
 };
 
 export default PaymentSuccess;
